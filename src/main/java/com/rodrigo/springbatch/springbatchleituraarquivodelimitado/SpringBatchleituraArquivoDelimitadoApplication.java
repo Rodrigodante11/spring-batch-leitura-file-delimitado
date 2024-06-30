11837cb055d366ea6405d25eaf4e6de63b88a758
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-06-30 10:50:22 -0300

    start project

---
+++ src/main/java/com/rodrigo/springbatch/springbatchleituraarquivodelimitado/SpringBatchleituraArquivoDelimitadoApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.springbatch.springbatchleituraarquivodelimitado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchleituraArquivoDelimitadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchleituraArquivoDelimitadoApplication.class, args);
+	}
+
+}
